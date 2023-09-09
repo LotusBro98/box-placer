9a40499c76eace169b79da74125420122958ee63
Andrey Isachenko <andrey.isachenko@bostongene.com>
2023-09-09 16:37:45 +0400

    small fixes in formulas

--- validation/formulas.py
+++ validation/formulas.py
@@ -1,38 +1,38 @@
 from validation.models import platform, boxes
-from validation.standarts import LONGITUDINAL_MAX_CG_BY_WEIGHT, MAX_HEIGHT
+from validation.standarts import calculate_long_max_cg_by_weight, MAX_HEIGHT
 
-print("Габариты грузов не выходят за пределы")
+print("Габариты грузов: не выходят за пределы")
 
-l_c_shipments = 0.5 * platform.floor_length - sum(box.weight * box.coords_of_cg[0] for box in boxes) / sum(
-    box.weight for box in boxes)
-print("1. Смещение ЦТ грузов в вагоне:", round(l_c_shipments), "мм <", LONGITUDINAL_MAX_CG_BY_WEIGHT[15], "мм")
-left_longitudinal_max_permissible_bias = 1
-right_longitudinal_max_permissible_bias = 2
-longitudinal_max_permissible_bias = 3
-is_longitudinal_bias_permissible = l_c_shipments < longitudinal_max_permissible_bias
-print("Смещение:", "допустимое" if is_longitudinal_bias_permissible else "НЕ ДОПУСТИМОЕ!")
-
+boxes_weight = round(sum(box.weight for box in boxes), 3)
+l_c_shipments = 0.5 * platform.floor_length - sum(box.weight * box.coords_of_cg[0] for box in boxes) / boxes_weight
+max_permissible_l_c_shipments = calculate_long_max_cg_by_weight(boxes_weight)
+print(f"1. Смещение ЦТ грузов в вагоне:\n"
+      f"Продольное смещение:\n"
+      f"0.5*{platform.floor_length} - ({' + '.join(f'{box.weight}*{box.coords_of_cg[0]}' for box in boxes)})/ {boxes_weight}")
+print("1. Смещение ЦТ грузов в вагоне:", round(l_c_shipments), "мм <", max_permissible_l_c_shipments, "мм")
+is_longitudinal_bias_permissible = l_c_shipments < max_permissible_l_c_shipments
+print("Смещение:", "допустимо" if is_longitudinal_bias_permissible else "НЕ ДОПУСТИМО!")
+print()
 l_c_overall = 0.5 * platform.floor_length - (
             sum((box.weight * box.coords_of_cg[0]) for box in boxes) + platform.weight * platform.length_to_cg) / (
                           sum(box.weight for box in boxes) + platform.weight)
 print("Продольное смещение грузов с вагоном:", round(l_c_overall), " мм")
-
+print()
 h_shipments_overall = sum(box.weight * (box.h_of_cg + platform.height_from_rails) for box in boxes) / sum(
     box.weight for box in boxes)
+# print(
+#     f"h_shipments_overall = sum({' + '.join(f'{box.weight}*({box.h_of_cg}+{platform.height_from_rails})' for box in boxes)}/{sum(box.weight for box in boxes)}")
 print(
-    f"h_shipments_overall = sum({' + '.join(f'{box.weight}*({box.h_of_cg}+{platform.height_from_rails})' for box in boxes)}/{sum(box.weight for box in boxes)}")
-print(
-    f"h_shipments_overall = sum({' + '.join(f'{box.weight}*({box.h_of_cg + platform.height_from_rails})' for box in boxes)}/{sum(box.weight for box in boxes)}")
-print("Общая высота ЦТ грузов в вагоне:", round(h_shipments_overall), "<", MAX_HEIGHT)
-print("Критерий: ", h_shipments_overall < MAX_HEIGHT)
-
+    f"2. Общая высота ЦТ грузов в вагоне: H_ct = sum({' + '.join(f'{box.weight}*{box.h_of_cg + platform.height_from_rails}' for box in boxes)}/{sum(box.weight for box in boxes)}"
+    f"\n = {round(h_shipments_overall)}мм < {MAX_HEIGHT}")
+print()
 h_overall = (sum(box.weight * (box.h_of_cg + platform.height_from_rails) for box in
                  boxes) + platform.weight * platform.cg_height_from_rails) / (
                         sum(box.weight for box in boxes) + platform.weight)
+print("Общая высота ЦТ")
 print(f"({' + '.join(f'{box.weight} * {str(box.h_of_cg + platform.height_from_rails)}' for box in boxes)} "
       f"+ {platform.weight} * {platform.cg_height_from_rails}) /"
-      f" ({' + '.join(str(box.weight) for box in boxes)} + {platform.weight})")
-print("Общая высота ЦТ:", round(h_overall), "<", MAX_HEIGHT)
+      f" ({' + '.join(str(box.weight) for box in boxes)} + {platform.weight})\n = {round(h_overall)}мм < {MAX_HEIGHT}мм")
 
 print("Устойчивость грузов с вагоном:")
 s_side_surface = sum(box.s_side_surface_meters for box in boxes) + platform.s_side_surface_meters
@@ -40,5 +40,5 @@ print(
     f"s_side_surface = {' + '.join(str(box.s_side_surface_meters) for box in boxes)} + {platform.s_side_surface_meters} = {s_side_surface}м2 < {50}м2")
 
 is_transverse_need_check = h_overall > MAX_HEIGHT or s_side_surface > 50
-print("Требуется ли проверка поперечной устойчивости груженого вагона:", is_transverse_need_check)
+print("Требуется ли проверка поперечной устойчивости груженого вагона:", "требуется" if is_transverse_need_check else "не требуется")
 # TODO: вычислить поперечную устойчивость, если требуется
--- validation/standarts.py
+++ validation/standarts.py
@@ -1,5 +1,46 @@
-# TODO: определять линейной интерполяцией промежуточные значения!
-# TODO: дозаполнить таблицу
-LONGITUDINAL_MAX_CG_BY_WEIGHT = {15: 2480}  # tons: millimeters
-
 MAX_HEIGHT = 2300  # millimeters
+
+
+LONGITUDINAL_MAX_CG_BIAS_DATA = (  # Масса груза, т     Максимальное смещение центра тяжести, мм
+"""10	3000
+15	2480
+20	2230
+25	2070
+30	1970
+35	1890
+40	1840
+45	1800
+50	1700
+55	1330
+60	860
+62	690
+67	300
+70	110
+>70	100""")
+
+raw_LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT = [tuple(row.split("\t")) for row in LONGITUDINAL_MAX_CG_BIAS_DATA.split("\n")[:-1]]  # tons: millimeters
+LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT = {}
+for weight, max_bias in raw_LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT:
+    LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT[float(weight)] = float(max_bias)
+
+LAST_MAX_CG_BY_WEIGHT = 100
+
+
+def calculate_long_max_cg_by_weight(weight: float) -> float:
+    if weight in LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT:
+        return LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT[weight]
+    elif weight < 10:
+        return LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT[10.0]
+    elif weight > 70:
+        return LAST_MAX_CG_BY_WEIGHT
+    else:
+        left_border_weight = None
+        right_border_weight = None
+        for index, current_weight in enumerate(LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT.keys()):
+            if weight > current_weight:
+                left_border_weight = current_weight
+                right_border_weight = list(LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT.keys())[index+1]
+        left_border_bias = LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT[left_border_weight]
+        right_border_bias = LONGITUDINAL_MAX_CG_BIAS_BY_WEIGHT[right_border_weight]
+        interpolated_bias = left_border_bias - (left_border_bias-right_border_bias)*(weight-left_border_weight)/(right_border_weight-left_border_weight)
+        return interpolated_bias
