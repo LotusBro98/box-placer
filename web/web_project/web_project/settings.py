309fbf4f126fc1369886be9f34ccbd7e007ee6f0
Lotusbro98 <lotusbro98@gmail.com>
2023-09-10 07:59:41 +0300

    created ready database with only carriage example

--- web/web_project/web_project/settings.py
+++ web/web_project/web_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # Директория, где хранятся ваши статические файлы в проекте
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'web_app/static'),
 ]
 
 # Default primary key field type
