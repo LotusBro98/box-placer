4b4696e895bc7b1a15c3a45813d44f285d5bed39
Lotusbro98 <lotusbro98@gmail.com>
2023-09-10 08:56:43 +0300

    draw centers of drawing

--- documentgen/drawing.py
+++ documentgen/drawing.py
@@ -21,6 +21,39 @@ class DrawColor:
     CENTER_OF_MASS_COLOR = "#ff0000"
 
 
+def draw_marker(scene: "Scene", pos):
+    radius = 0.05 * scene.scale
+    circle = draw.Circle(
+        pos[0], pos[1], radius,
+        stroke=DrawColor.CENTER_OF_MASS_COLOR, stroke_width=3, fill="transparent"
+    )
+    line1 = draw.Line(
+        pos[0], pos[1] - radius, pos[0], pos[1] + radius,
+        stroke=DrawColor.CENTER_OF_MASS_COLOR, stroke_width=3, fill="transparent"
+    )
+    line2 = draw.Line(
+        pos[0] - radius, pos[1], pos[0] + radius, pos[1],
+        stroke=DrawColor.CENTER_OF_MASS_COLOR, stroke_width=3, fill="transparent"
+    )
+    scene.drawing.append(circle)
+    scene.drawing.append(line1)
+    scene.drawing.append(line2)
+
+
+def draw_label(scene: "Scene", pos, text, color=DrawColor.BOX_COLOR, style="italic"):
+    offset = np.array([0.1, -0.1]) * scene.scale
+    pos += offset
+    size = 0.2 * scene.scale
+    text_fig = draw.Text(text, size, pos[0], pos[1], stroke=color, font_family="Arial", font_style=style)
+    scene.drawing.append(text_fig)
+
+
+def get_center_of_mass(elements):
+    center = np.sum([(elem.pos + elem.center_of_mass) * elem.mass for elem in elements], axis=0)
+    center /= np.sum([elem.mass for elem in elements], axis=0)
+    return center
+
+
 class DrawBox:
     pos: np.ndarray             # meters
     center_of_mass: np.ndarray  # meters, relative to pos
@@ -50,38 +83,14 @@ class DrawBox:
 
     def draw_center(self, scene: "Scene"):
         pos = scene.project(self.pos + self.center_of_mass)
-
-        radius = 0.05 * scene.scale
-        circle = draw.Circle(
-            pos[0], pos[1], radius,
-            stroke=DrawColor.CENTER_OF_MASS_COLOR, stroke_width=3, fill="transparent"
-        )
-        line1 = draw.Line(
-            pos[0], pos[1] - radius, pos[0], pos[1] + radius,
-            stroke=DrawColor.CENTER_OF_MASS_COLOR, stroke_width=3, fill="transparent"
-        )
-        line2 = draw.Line(
-            pos[0] - radius, pos[1], pos[0] + radius, pos[1],
-            stroke=DrawColor.CENTER_OF_MASS_COLOR, stroke_width=3, fill="transparent"
-        )
-        scene.drawing.append(circle)
-        scene.drawing.append(line1)
-        scene.drawing.append(line2)
+        draw_marker(scene, pos)
+        draw_label(scene, pos, f"ЦТгр{self.id}")
 
     def draw_dimension(self, scene: "Scene"):
         center = scene.project(self.pos)
         dims = scene.project_size(self.dimensions)
         left = center[0] - dims[0] / 2
         right = center[0] + dims[0] / 2
-
-    def draw_label(self, scene: "Scene"):
-        pos = scene.project(self.pos + self.center_of_mass)
-        text = f"ЦТгр{self.id}"
-        offset = np.array([0.1, -0.1]) * scene.scale
-        pos += offset
-        size = 0.2 * scene.scale
-        text_fig = draw.Text(text, size, pos[0], pos[1], stroke=DrawColor.BOX_COLOR, font_family="Arial", font_style="italic")
-        scene.drawing.append(text_fig)
 
     def draw_box(self, scene: "Scene"):
         pos = scene.project(self.pos)
@@ -93,12 +102,13 @@ class DrawBox:
     def draw(self, scene: "Scene"):
         self.draw_box(scene)
         self.draw_center(scene)
-        self.draw_label(scene)
 
 
 class Platform:
-    pos = np.array([0, 0, -0.1])
+    pos = np.array([0, 0, -1e-9])
     dimensions: np.ndarray
+    center_of_mass: np.ndarray
+    mass: float
 
     side_points = np.array([[-0.5, 0], [0.5, 0], [0.5, 0.3], [0.25, 0.3], [0.15, 0.6], [-0.15, 0.6], [-0.25, 0.3], [-0.5, 0.3]])
     top_points = np.array([[-0.5, -0.5], [0.5, -0.5], [0.5, 0.5], [-0.5, 0.5]])
@@ -107,15 +117,25 @@ class Platform:
     front_wheel2 = np.array([[0.3, 0.3], [0.2, 0.3], [0.2, 1], [0.3, 1]])
     side_wheels = np.array([[-0.43, 0.65], [-0.30, 0.65], [0.30, 0.65], [0.43, 0.65]])
 
-    def __init__(self, wx, wy, wz):
-        self.dimensions = np.array([wx, wy, wz])
+    def __init__(self, dimensions, center_of_mass, mass):
+        self.dimensions = np.array(dimensions)
+        self.center_of_mass = np.array(center_of_mass)
+        self.mass = mass
 
     @staticmethod
     def from_carriage(carriage: Carriage):
         return Platform(
-            wx=carriage.floor_width / 1000,
-            wy=carriage.floor_length / 1000,
-            wz=carriage.height_from_rails / 1000
+            dimensions=[
+                carriage.floor_width / 1000,
+                carriage.floor_length / 1000,
+                carriage.height_from_rails / 1000,
+            ],
+            center_of_mass=[
+                0,
+                (carriage.length_to_cg - carriage.floor_length / 2) / 1000,
+                (carriage.cg_height_from_rails - carriage.height_from_rails) / 1000,
+            ],
+            mass=float(carriage.weight * 1000)
         )
 
     def draw_side(self, scene: "Scene"):
@@ -147,6 +167,11 @@ class Platform:
         scene.drawing.append(polygon_wheel1)
         scene.drawing.append(polygon_wheel2)
 
+    def draw_center(self, scene: "Scene"):
+        pos = scene.project(self.center_of_mass)
+        draw_marker(scene, pos)
+        draw_label(scene, pos, f"ЦТв", color=DrawColor.CENTER_OF_MASS_COLOR)
+
     def draw(self, scene: "Scene"):
         draw_fns = {
             Projection.SIDE: self.draw_side,
@@ -156,6 +181,7 @@ class Platform:
         }
         draw_fn = draw_fns[scene.projection]
         draw_fn(scene)
+        # self.draw_center(scene)
 
 
 class Scene:
@@ -190,6 +216,20 @@ class Scene:
     def set_projection(self, projection):
         self.projection = projection
 
+    def draw_centers(self):
+        center_of_mass_boxes = get_center_of_mass([elem for elem in self.elements if isinstance(elem, DrawBox)])
+        center_of_mass_boxes = self.project(center_of_mass_boxes)
+        draw_marker(self, center_of_mass_boxes)
+        draw_label(self, center_of_mass_boxes, "ЦТгр_о", color=DrawColor.PLATFORM_COLOR, style="bold")
+
+        center_of_mass_all = get_center_of_mass(self.elements)
+        center_of_mass_all = self.project(center_of_mass_all)
+        draw_marker(self, center_of_mass_all)
+        draw_label(self, center_of_mass_all, "ЦТо", color=DrawColor.PLATFORM_COLOR, style="bold")
+
+        # Draw platform center of mass
+        [e for e in self.elements if isinstance(e, Platform)][0].draw_center(self)
+
     def draw_projection(self, projection, origin):
         elems = [elem for elem in self.elements]
         elems = sorted(elems, key=lambda e: e.pos[np.array(projection.value[0])][2] * projection.value[1][2], reverse=True)
@@ -197,6 +237,7 @@ class Scene:
         self.set_projection(projection)
         for elem in elems:
             elem.draw(self)
+        self.draw_centers()
 
     def draw(self, f=None):
         self.drawing.clear()
@@ -240,10 +281,9 @@ def main():
         Box(coords_of_cg=(6930, 0, 1150 / 2), dimensions=(4100, 1720, 1150), weight=1.865),
     ]
 
-    f_bytes = generate_drawing(platform, boxes)
+    b = generate_drawing(platform, boxes)
     with open("drawing.svg", "wb+") as f:
-        f_bytes.seek(0)
-        shutil.copyfileobj(f_bytes, f)
+        f.write(b)
 
 
 if __name__ == '__main__':
